8eeeebae10d75789640a178eb530d8a5564b039c
veenavikas <veenavikas17@gmail.com>
2026-05-21 16:19:15 +0530

    feat: integrate Google Tag Manager, GA4 tracking codes, and Google site verification

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import "./globals.css";
 import Navbar from "@/components/Navbar";
 import SmoothScroll from "@/components/SmoothScroll";
 import WhatsAppWidget from "@/components/WhatsAppWidget";
+import Script from "next/script";
 
 export const metadata: Metadata = {
   title: "Sushrutha Nature Cure | Ancestral Wisdom. Modern Equilibrium.",
@@ -43,6 +44,39 @@ export default function RootLayout({
         <link rel="icon" href="/assets/susrutha_logo.png" type="image/png" />
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-N2P4WT74"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
+
+        {/* Google Tag Manager */}
+        <Script id="gtm-script" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-N2P4WT74');`}
+        </Script>
+
+        {/* Google tag (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-FXKEBDGHHZ"
+          strategy="afterInteractive"
+        />
+        <Script id="ga-script" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-FXKEBDGHHZ');
+          `}
+        </Script>
+
         <div className="texture-overlay" aria-hidden="true" />
         <SmoothScroll>
           <Navbar />
